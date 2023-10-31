166d14734af2dc4588a56751cdea69303a342aaa
Nutsang <foxteary@gmail.com>
2023-10-31 23:31:15 +0700

    V6

--- models/home-model.js
+++ models/home-model.js
@@ -4,10 +4,8 @@ module.exports.getMedia = (request, response) => {
     connection.query('SELECT COUNT(id) as media_amount FROM media', (error, result) => {
         if (error) response.status(400).json({message: 'แสดงข้อมูลล้มเหลว'})
         if (result[0].amount <= 0) response.status(400).json({message: 'แสดงข้อมูลล้มเหลว'})
-        
         connection.query('SELECT media.*, GROUP_CONCAT(DISTINCT media_genre.genre ORDER BY media_genre.genre ASC) AS genres, COUNT(episode.id) AS episode_amount FROM media LEFT JOIN media_genre ON media.id = media_genre.media_id LEFT JOIN episode ON media.id = episode.media_id GROUP BY media.id, media.title ORDER BY media.popularity DESC', (error, result) => {
             response.status(200).json(result)
         })
-        
     })
 }
